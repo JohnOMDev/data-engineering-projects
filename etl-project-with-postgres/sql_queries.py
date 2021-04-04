52f911c82246f26585167c8f43872387b3ee92b5
JohnOMDev <omolejohna@gmail.com>
2021-04-05 00:42:27 +0200

    update

--- etl-project-with-postgres/sql_queries.py
+++ etl-project-with-postgres/sql_queries.py
@@ -9,25 +9,33 @@ time_table_drop = "DROP TABLE IF EXISTS songplays"
 # CREATE TABLES
 
 songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays(songplay_id bigint, \
-                             start_time timestamp, user_id int, level varchar, song_id varchar, \
-                             artist_id varchar, session_id int, location varchar, \
-                                 user_agent varchar)
+                             start_time timestamp NOT NULL, \
+                             user_id int REFERENCES users (user_id), \
+                                 level varchar, song_id varchar, \
+                                artist_id varchar, session_id int, \
+                                    location varchar(50) NOT NULL, \
+                                        user_agent varchar, PRIMARY KEY (songplay_id, session_id))
+
 """)
 
-user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, \
-                         last_name varchar, gender varchar, level varchar)
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar(45) \
+                     NOT NULL, last_name varchar(45) NOT NULL, gender varchar, level varchar, \
+                             PRIMARY KEY (user_id));
 """)
 
 song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, \
-                         artist_id varchar, year int, duration decimal(20,10))
+                         artist_id varchar, year int NOT NULL, duration decimal(20,10) NOT NULL, \
+                             PRIMARY KEY(song_id));
 """)
 
 artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, \
-                           location varchar, latitude decimal(10,2), longitude decimal(20,10))
+                           location varchar, latitude decimal(10,2), longitude decimal(20,10), \
+                               PRIMARY KEY(artist_id));
 """)
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, \
-                         day int, week int, month int, year int, weekday varchar)
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time timestamp NOT NULL, \
+                     hour int NOT NULL, day int NOT NULL, week int NOT NULL, month int NOT NULL, \
+                         year int NOT NULL, weekday varchar NOT NULL);
 """)
 
 # INSERT RECORDS
@@ -39,20 +47,26 @@ songplay_table_insert = (""" INSERT INTO songplays(songplay_id, \
 """)
 
 user_table_insert = (""" INSERT INTO users (user_id, first_name, \
-                         last_name, gender, level) VALUES(%s, %s, %s, %s, %s)
+                         last_name, gender, level) VALUES(%s, %s, %s, %s, %s) \
+                            ON CONFLICT(user_id) \
+                            DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = (""" INSERT INTO songs (song_id, title, \
-                         artist_id, year, duration) VALUES(%s, %s, %s, %s, %s)
+                         artist_id, year, duration) VALUES(%s, %s, %s, %s, %s) \
+                            ON CONFLICT (song_id) \
+                            DO NOTHING;
 """)
 
 artist_table_insert = (""" INSERT INTO artists (artist_id, name, \
-                           location, latitude, longitude) VALUES(%s, %s, %s, %s, %s)
+                           location, latitude, longitude) VALUES(%s, %s, %s, %s, %s) \
+                            ON CONFLICT (artist_id) \
+                            DO NOTHING;
 """)
 
 
 time_table_insert = (""" INSERT INTO time (start_time, hour, \
-                         day, week, month, year, weekday) VALUES(%s, %s, %s, %s, %s, %s, %s)
+                         day, week, month, year, weekday) VALUES(%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
@@ -64,7 +78,7 @@ song_select = (""" SELECT artists.artist_id, songs.song_id FROM (songs JOIN arti
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create,
-                        artist_table_create, time_table_create]
+create_table_queries = [user_table_create, artist_table_create, song_table_create,
+                        time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop,
                       artist_table_drop, time_table_drop]

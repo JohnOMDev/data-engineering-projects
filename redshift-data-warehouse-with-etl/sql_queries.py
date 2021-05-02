0cd15c8ea85b8327f326965c44266ad0ce21276e
JohnOMDev <omolejohna@gmail.com>
2021-05-02 16:47:57 +0200

    updates

--- redshift-data-warehouse-with-etl/sql_queries.py
+++ redshift-data-warehouse-with-etl/sql_queries.py
@@ -183,6 +183,7 @@ song_table_insert = ("""
   year                 AS year,
   duration             AS duration
   FROM staging_songs
+WHERE song_id NOT IN (SELECT DISTINCT song_id FROM songs)
 """)
 
 artist_table_insert = ("""
@@ -194,6 +195,7 @@ artist_table_insert = ("""
          artist_latitude    AS latitude,
          artist_longitude   AS longitude
   FROM staging_songs
+WHERE artist_id NOT IN (SELECT DISTINCT artist_id FROM artists)
 """)
 
 time_table_insert = ("""

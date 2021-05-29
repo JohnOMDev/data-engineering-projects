8434f56f1178c48f6dfc8487c8276e801c919b72
JohnOMDev <omolejohna@gmail.com>
2021-05-29 23:31:56 +0200

    updatind the etl

--- creating_timeseries_etl/sql_queries.py
+++ creating_timeseries_etl/sql_queries.py
@@ -11,13 +11,13 @@ sales_table_drop = "DROP TABLE IF EXISTS sales"
 
 
 # CREATE TABLES
-crm_table_create = (""" CREATE TABLE IF NOT EXISTS crm (crm_id SERIAL, account_id varchar(250), \
+crm_table_create = (""" CREATE TABLE IF NOT EXISTS crm (crm_id SERIAL, account_id varchar(250) NOT NULL,
                          event_type varchar(200) NOT NULL, event_date date NOT NULL,
                          PRIMARY KEY(crm_id))
 """)
 
 
-sale_table_create = (""" CREATE TABLE IF NOT EXISTS sales (sale_id SERIAL, account_id varchar(250),
+sale_table_create = (""" CREATE TABLE IF NOT EXISTS sales (sale_id SERIAL, account_id varchar(250)  NOT NULL,
                             product_name varchar(100) NOT NULL, date date NOT NULL,
                             unit_sales int NOT NULL, created_time timestamp NOT NULL,
                             PRIMARY KEY(sale_id))
@@ -25,15 +25,15 @@ sale_table_create = (""" CREATE TABLE IF NOT EXISTS sales (sale_id SERIAL, accou
 
 
 # INSERT RECORDS
-crm_table_insert = (""" INSERT INTO crm (account_id, event_type, \
-                         event_date) VALUES(%s, %s, %s) \
-                                ON CONFLICT (crm_id) \
+crm_table_insert = (""" INSERT INTO crm (account_id, event_type,
+                         event_date) VALUES(%s, %s, %s)
+                                ON CONFLICT (crm_id)
                                     DO NOTHING;
 """)
 
 sales_table_insert = (""" INSERT INTO sales (account_id, product_name, date, unit_sales,
-                      created_time) VALUES(%s, %s, %s, %s, %s) \
-                                ON CONFLICT (sale_id) \
+                      created_time) VALUES(%s, %s, %s, %s, %s)
+                                ON CONFLICT (sale_id)
                                     DO NOTHING;
 """)
 
